22a7a1aca3c0cb43f93298b6989800c0362daada
Laura Angélica Valadez Rojas <lauris2628@hotmail.com>
2016-03-19 20:53:37 +0000

    change button quote

--- views/quote_form.php
+++ views/quote_form.php
@@ -3,7 +3,11 @@
         <div class="form-group">
             <input autofocus class="form-control" name="symbol" placeholder="symbol" type="text"/>
         </div>
-            <button type="submit" class="btn btn-default">Quote</button>
+            
+            <button type="sybmit" class="btn btn-default btn-lg">
+              <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Quote
+            </button>
+
         </div>
     </fieldset>
 </form>
